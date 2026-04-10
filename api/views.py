88a05184a853b15f26802d7740032298d3e05748
Kostushkina <bimochekzaika@gmail.com>
2026-04-10 21:29:58 +0300

    Add rating API endpoint

--- api/views.py
+++ api/views.py
@@ -1,5 +1,26 @@
-from django.http import HttpResponse
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Student, Event, Request
+from .serializers import StudentSerializer, EventSerializer, RequestSerializer
 
 
-def index(request):
-    return HttpResponse("Hello, world!")
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    @action(detail=False, methods=['get'])
+    def rating(self, request):
+        students = Student.objects.all().order_by('-total_points')
+        serializer = self.get_serializer(students, many=True)
+        return Response(serializer.data)
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class RequestViewSet(viewsets.ModelViewSet):
+    queryset = Request.objects.all()
+    serializer_class = RequestSerializer
